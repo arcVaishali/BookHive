609eb229153d31ca7b6e83295666e0b6c6ecdf35
vaishali <vaishalirdj14@gmail.com>
2024-02-10 17:18:24 +0530

    matched books gallery page designed

--- frontend/src/Pages/MatchedBooksGallery.jsx
+++ frontend/src/Pages/MatchedBooksGallery.jsx
@@ -9,7 +9,38 @@ import BookClub from "../components/BookClub";
 const MatchedBooksGallery = () => {
   return (
     <>
-    
+    <div className="grid grid-cols-12 col-span-12 m-8 p-16 gap-2">
+        <BookCard
+          image={bookdummyimage}
+          title="Vaishali"
+          author="Vaishali"
+          price="134"
+        />
+      </div>
+      <div className="grid grid-cols-12 col-span-12 m-4 p-16 gap-2">
+        <BookCardWide
+          image={dummyimage}
+          title="Vaishali"
+          author="Vaishali"
+          text="xyz"
+        />
+      </div>
+      <div className="grid grid-cols-12 col-span-12 m-4 p-16 gap-2">
+        <BookCardPortrait
+          image={bookdummyimage}
+          title="Vaishali"
+          author="Vaishali"
+          text="xyz"
+        />
+      </div>
+      <div className="grid grid-cols-12 col-span-12 m-4 p-16 gap-2">
+        <BookClub
+          image={profile}
+          name="Vaishali"
+          tagline="Amet deserunt sint proident"
+          text="Ex pariatur elit id nulla consequat.Veniam excepteur duis pariatur elit duis consectetur et consectetur."
+        />
+      </div>
     </>
   );
 };
