608bbe603ed3d189a96e22dbff7253b63f62806a
vaishali <vaishalirdj14@gmail.com>
2024-02-11 19:50:12 +0530

    matched books gallery made

--- frontend/src/Pages/MatchedBooksGallery.jsx
+++ frontend/src/Pages/MatchedBooksGallery.jsx
@@ -8,6 +8,9 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import { recommendedBooks } from "../constants/bookcard";
 import { recentMatchedBooks } from "../constants/bookcard";
+import { lastBorrowedBooks } from "../constants/bookcard";
+import { highlights } from "../constants/bookcard";
+import { bookClubs } from "../constants/bookcard";
 
 const MatchedBooksGallery = () => {
   const responsive = {
@@ -31,7 +34,7 @@ const MatchedBooksGallery = () => {
   return (
     <>
       <div className="grid grid-cols-12 col-span-12 m-8 p-16 gap-2">
-        {/* <div className="col-span-12 text-5xl uppercase font-semibold">Top Picks</div> */}
+        <div className="col-span-12 text-5xl font-bold ml-4">Top Picks</div>
         <Carousel
           className="col-span-12"
           swipeable={true}
@@ -61,7 +64,10 @@ const MatchedBooksGallery = () => {
       </div>
 
       <div className="grid grid-cols-12 col-span-12 m-4 p-16 gap-2">
-        <div className="flex col-span-12 text-5xl font-bold m-4">Recent</div>
+        <div className="flex col-span-12 text-5xl font-bold m-4">
+          Recent
+          {/* search bar */}
+        </div>
         {recentMatchedBooks.map((book) => (
           <BookCardWide
             image={book.image}
@@ -72,20 +78,55 @@ const MatchedBooksGallery = () => {
         ))}
       </div>
       <div className="grid grid-cols-12 col-span-12 m-4 p-16 gap-4">
-        <BookCardPortrait
-          image={bookdummyimage}
-          title="Vaishali"
-          author="Vaishali"
-          text="xyz"
-        />
+        <div className="grid grid-cols-6 flex-wrap col-span-8">
+          {lastBorrowedBooks.map((book) => (
+            <BookCard
+              image={book.image}
+              title={book.title}
+              author={book.author}
+              text={book.text}
+            />
+          ))}
+        </div>
+        <div className="grid gap-4 col-span-4">
+          {highlights.map((book) => (
+            <BookCardPortrait
+              image={book.image}
+              title={book.title}
+              author={book.author}
+              text={book.text}
+            />
+          ))}
+        </div>
       </div>
       <div className="grid grid-cols-12 col-span-12 m-4 p-16 gap-2">
-        <BookClub
-          image={profile}
-          name="Vaishali"
-          tagline="Amet deserunt sint proident"
-          text="Ex pariatur elit id nulla consequat.Veniam excepteur duis pariatur elit duis consectetur et consectetur."
-        />
+        <div className="text-5xl font-bold col-span-12 ml-4">Book Clubs</div>
+        <Carousel
+          className="col-span-12"
+          swipeable={true}
+          draggable={true}
+          showDots={true}
+          responsive={responsive}
+          ssr={true} // means to render carousel on server-side.
+          infinite={true}
+          autoPlaySpeed={1000}
+          keyBoardControl={true}
+          customTransition="all .5"
+          transitionDuration={500}
+          containerClass="carousel-container"
+          removeArrowOnDeviceType={["tablet", "mobile"]}
+          dotListClass="custom-dot-list-style"
+          itemClass="carousel-item-padding-40-px"
+        >
+          {bookClubs.map((book) => (
+            <BookClub
+              image={book.image}
+              name={book.name}
+              tagline={book.tagline}
+              text={book.text}
+            />
+          ))}
+        </Carousel>
       </div>
     </>
   );
--- frontend/src/components/BookCardPortrait.jsx
+++ frontend/src/components/BookCardPortrait.jsx
@@ -11,10 +11,10 @@ const BookCardPortrait = (props) => {
       <div className="grid grid-cols-6 p-2 ">
         <div className="col-span-3 grid grid-rows-1">
           <div className="text-xl font-bold">{props.title}</div>
-          <p>{props.author}</p>
+          <p className="text-gray-500 front-extralight text-sm">{props.author}</p>
         </div>
         <div className="col-span-3 grid grid-rows-1">
-          <div className="m-[4px]">{props.text}</div>
+          <div className="text-xs text-gray-400 m-2">{props.text}</div>
           <PrimaryButton text="Read more" location="/book/2" />
         </div>
       </div>
