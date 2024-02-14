839074df3c2d151cfe8edeee727d0f943acc3ddd
vaishali <vaishalirdj14@gmail.com>
2024-02-14 22:44:44 +0530

    swiper gallery

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MatchedBooksGallery from "./Pages/MatchedBooksGallery";
 import Home from "./Pages/Home";
 import UserDashboard from "./Pages/UserDashboard";
+import Swiper from "./Pages/Swiper";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
             <Route path="/" element={<Home/>} />
             <Route path="/books-gallery" element={<MatchedBooksGallery/>} />
             <Route path="/user" element={<UserDashboard/>} />
+            <Route path="/swipe" element={<Swiper />} />
           </Routes>
         </BrowserRouter>
       </>
--- frontend/src/Pages/Swiper.jsx
+++ frontend/src/Pages/Swiper.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import TinderCard from "react-tinder-card";
+import BookCard from "../components/BookCard";
+import { books } from "../constants/bookcard";
+
+const onSwipe = (direction) => {
+  console.log("You swiped: " + direction);
+};
+
+const onCardLeftScreen = (myIdentifier) => {
+  console.log(myIdentifier + " left the screen");
+};
 
 const Swiper = () => {
   return (
-    <div className='grid grid-cols-12 col-span-12'>
-
+    <div className="grid grid-cols-12 col-span-12">
+      <TinderCard
+    //   className="cols-span-12"
+        onSwipe={onSwipe}
+        onCardLeftScreen={() => onCardLeftScreen("fooBar")}
+        // preventSwipe={["right", "left"]}
+      >
+        {books.map((book) => (
+          <BookCard
+            image={book.image}
+            title={book.title}
+            author={book.author}
+            text={book.text}
+          />
+        ))}
+      </TinderCard>
     </div>
-  )
-}
+  );
+};
 
-export default Swiper
+export default Swiper;
